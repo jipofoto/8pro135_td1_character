8d67f4337017ca9d1d35002d110358c452de3f8e
Jean-Philippe Tremblay <133444502+jipofoto@users.noreply.github.com>
2024-10-10 23:40:35 -0400

    Compile avec les fonctions mis en place mais problem d'accès au GetTree() donc on passe par Game.cs : node ; ;

--- scripts/Game.cs
+++ scripts/Game.cs
@@ -1,17 +1,40 @@
-// using Godot;
-// using System;
-//
-// public partial class Game : Node
-// {
-// 	// Called when the node enters the scene tree for the first time.
-// 	public override void _Ready()
-// 	{
-// 		GD.Print("Game Ready");
-// 	}
-// 	
-// 	// Called every frame. 'delta' is the elapsed time since the previous frame.
-// 	public override void _Process(double delta)
-// 	{
-// 		
-// 	}
-// }
+using Godot;
+using System;
+
+public partial class Game : Node
+{
+	private Button saveButton;
+	private SaveManager saveManager;
+
+	public override void _Ready()
+	{
+		GD.Print("Game Ready");
+		saveButton = GetNode<Button>("SaveButton");
+		saveManager = GameManager.Get().GetSaveManager();
+		saveButton.Pressed += _on_save_pressed;
+	}
+
+	private void _on_save_pressed()
+	{
+		GD.Print("Save Button pressed");
+		saveManager.SaveGame();
+	}
+
+	private void _on_load_pressed()
+	{
+		GD.Print("Load Button pressed");
+		saveManager.LoadGame();
+	}
+
+	private void _on_quit_pressed()
+	{
+		GD.Print("Quit Button pressed");
+		GetTree().Quit();
+	}
+	
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+		
+	}
+}
