f18bcd9b2d93bda253457e4e0a5a2e60392d6de4
Jean-Philippe Tremblay <133444502+jipofoto@users.noreply.github.com>
2024-10-10 21:35:35 -0400

    Test save menu

--- scripts/GameManager.cs
+++ scripts/GameManager.cs
@@ -5,12 +5,14 @@ using System;
 public partial class GameManager : SceneTree
 {
 	private double timeElapsed = 0;
-	
+	private bool isMenuOpen = false;
+
 	private static GameManager instance;
 	private LevelManager levelManager;
 	private SaveManager saveManager;
+	private CanvasLayer menuCanvasLayer;
 
-	private string scenePath = "res://Scenes/test.tscn";
+	private string scenePath;
 
 	public static GameManager Get()
 	{
@@ -43,18 +45,35 @@ public partial class GameManager : SceneTree
 	{
 		get { return scenePath; }
 		set { scenePath = value; }
-	}	
+	}
+
 	public override void _Initialize()
 	{
 		GD.Print("Initialized:");
 		GD.Print($"  Starting Time: {timeElapsed}");
 		levelManager = GetLevelManager();
 		levelManager.LoadLevel(scenePath);
-	}
 
+		// menuCanvasLayer = GetTree().Root.GetNode<CanvasLayer>("MenuCanvasLayer");
+		menuCanvasLayer.Hide();
+	}
 	public override bool _Process(double delta)
 	{
 		timeElapsed += delta;
+
+		if (Input.IsKeyPressed(Key.Escape))
+		{
+			isMenuOpen = !isMenuOpen;
+			if (isMenuOpen)
+			{
+				menuCanvasLayer.Show();
+			}
+			else
+			{
+				menuCanvasLayer.Hide();
+			}
+		}
+
 		// Return true to end the main loop.
 		return Input.GetMouseButtonMask() != 0 || Input.IsKeyPressed(Key.Escape);
 	}
@@ -63,5 +82,24 @@ public partial class GameManager : SceneTree
 	{
 		GD.Print("Finalized:");
 		GD.Print($"  End Time: {timeElapsed}");
+	}
+
+	public void SaveGame()
+	{
+		saveManager.SaveGame();
+		isMenuOpen = false;
+		menuCanvasLayer.Hide();
+	}
+
+	public void LoadGame()
+	{
+		saveManager.LoadGame();
+		isMenuOpen = false;
+		menuCanvasLayer.Hide();
+	}
+
+	public void QuitGame()
+	{
+		// GetTree().Quit();
 	}
 }
